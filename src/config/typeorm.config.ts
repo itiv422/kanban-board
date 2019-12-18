7713185d052b6a0d907a8ad0a137425b46cae4ef
Tatsianenka Ilya <itiv422@gmail.com>
2019-12-19 00:53:45 +0300

    init

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'Admin',
+    password: 'r00t',
+    database: 'task_management',
+    entities: [__dirname + '/../**/*.entity.js'],
+    synchronize: true
+};
---
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -0,0 +1,13 @@
+
+import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
+import { TaskStatus } from "../task-status.enum";
+
+export class GetTasksFilterDto {
+    @IsOptional()
+    @IsIn(Object.keys(TaskStatus))
+    status: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
+    search: string;
+}
