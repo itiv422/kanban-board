31b77e066a5ea9a58cddde158a2fc7d6043e2d0b
Tatsianenka Ilya <itiv422@gmail.com>
2020-01-07 20:55:12 +0300

    Code rebase.

---
+++ webapp_data/static/src/components/NewQuote.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <div class="col-12 form-group">
+      <label>Quote</label>
+      <textarea class="form-control" rows="3" v-model="quote"></textarea>
+    </div>
+    <div class="offset-4 col-4 form-group">
+      <button class="btn btn-primary w-100" @click.prevent="createNew">Add Quote</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    createNew() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ webapp_data/static/src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="card">
+      <div class="card-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"]
+};
+</script>
+
+<style>
+.card-body {
+  font-size: 24px;
+  color: #6e6e6e;
+  cursor: pointer;
+}
+
+.card-body:hover {
+  background-color: #ffe2e2;
+}
+</style>
