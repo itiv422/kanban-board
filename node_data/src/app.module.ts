20d69c64373ca301221829cf76f2b23037d20235
Ilya Tatsianenka <Ilya_Tatsianenka@epam.com>
2020-01-10 19:07:30 +0300

    added redit

--- node_data/src/app.module.ts
+++ node_data/src/app.module.ts
@@ -6,13 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { QueueModule } from 'nest-queue';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     MongooseModule.forRoot(mongoUrl, mongoOptions),
-    QueueModule.forRoot({connection: { redis: { port: 6379, host: 'localhost'}}}),
     TasksModule,
     AuthModule,
     PhoneBookModule
--- node_data/src/tasks/services/solr.service.ts
+++ node_data/src/tasks/services/solr.service.ts
@@ -1,49 +1,60 @@
 import solr = require('solr-client');
 import { Injectable, Logger } from '@nestjs/common';
 import config = require('config');
+import redis = require('redis');
 
 @Injectable()
 export class SolrService {
     private readonly logger = new Logger('SolrService');
+    private readonly redisClient = redis.createClient({host: 'localhost', port: 6379});
 
-    addTask(task: any): boolean {
-        let isIndexed = true;
-        const solrClient = solr.createClient(config.get('solr'));
-        solrClient.add(task,
-            err => {
-                if (err) {
-                    isIndexed = false;
-                    solrClient.rollback();
-                    this.logger.error(JSON.stringify(err));
-                } else {
-                    solrClient.commit();
-                    this.logger.log(`New task in SOLR index. Task id: ${task.taskId}`);
-                }
-            }
-        );
+    async addTask(task: any) {
+        // let isIndexed = true;
+        // const solrClient = solr.createClient(config.get('solr'));
+        // solrClient.add(task,
+        //     err => {
+        //         if (err) {
+        //             isIndexed = false;
+        //             solrClient.rollback();
+        //             this.logger.error(JSON.stringify(err));
+        //         } else {
+        //             solrClient.commit();
+        //             this.logger.log(`New task in SOLR index. Task id: ${task.taskId}`);
+        //         }
+        //     }
+        // );
 
-        return isIndexed;
+        // return isIndexed;
+        this.redisClient.sadd('trololo2', JSON.stringify(task));
+
+        const elementCount: number = await new Promise(resolve => {
+          this.redisClient.scard('trololo2', (err, value) => resolve(value));
+        });
+
+        if (elementCount > 0) {
+          this.redisClient.spop('trololo2', elementCount, (err, values) => console.log(values));
+        }
     }
 
     deleteTaskByTaskId(taskId: number): void {
-        const solrClient = solr.createClient(config.get('solr'));
-        const field = 'taskId';
-        solrClient.delete(field, String(taskId),
-            err => {
-                if (err) {
-                    solrClient.rollback();
-                    this.logger.error(JSON.stringify(err));
-                } else {
-                    solrClient.commit();
-                    this.logger.log(`Delete task. Task id: ${taskId}`);
-                }
-            }
-        );
+        // const solrClient = solr.createClient(config.get('solr'));
+        // const field = 'taskId';
+        // solrClient.delete(field, String(taskId),
+        //     err => {
+        //         if (err) {
+        //             solrClient.rollback();
+        //             this.logger.error(JSON.stringify(err));
+        //         } else {
+        //             solrClient.commit();
+        //             this.logger.log(`Delete task. Task id: ${taskId}`);
+        //         }
+        //     }
+        // );
     }
 
     updateTaskStatusByTaskId(taskId: number, task: any): void {
-        this.deleteTaskByTaskId(taskId);
-        this.addTask(task);
+        // this.deleteTaskByTaskId(taskId);
+        // this.addTask(task);
     }
 
     async search(query: string, userName: string) {
